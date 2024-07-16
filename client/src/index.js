ca46cdc9226afb3b6a561b18a170035a2a38e8d4
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-07-15 20:29:14 -0600

    index.js setup, App wrapped with themeProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,27 @@
 // index.js
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './components/App.jsx';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
-import './style.css';
+
+const theme = createTheme();
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
+
+ // // index.js
+
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './components/App.jsx';
+// import './style.css';
+
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
