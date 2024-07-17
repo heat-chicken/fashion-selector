c19afb27675a0dad2125fa2dbf0a8a0968f75586
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-07-17 17:29:04 -0600

    got background img rendering and cleaned up App.jsx- ThemeProvider

--- client/src/components/Background.jsx
+++ client/src/components/Background.jsx
@@ -1,33 +1,68 @@
 // client/src/components/Background.jsx
 
+// Background.jsx
 import React from 'react';
 import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import backgroundImage from '../assets/images/background1.jpg';
 
-const BackgroundBox = styled(Box)(({ theme }) => ({
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  zIndex: -1,
-  '&::before': {
-    content: '""',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    opacity: 0.5,
-  },
-}));
-
 const Background = () => {
-  return <BackgroundBox />;
+  return (
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1,
+        '&::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 0.5,
+        },
+      }}
+    />
+  );
 };
 
 export default Background;
+
+
+// import React from 'react';
+// import { Box } from '@mui/material';
+// import { styled } from '@mui/material/styles';
+// import backgroundImage from '../assets/images/background1.jpg';
+
+// const BackgroundBox = styled(Box)(({ theme }) => ({
+//   position: 'fixed',
+//   top: 0,
+//   left: 0,
+//   right: 0,
+//   bottom: 0,
+//   zIndex: -1,
+//   '&::before': {
+//     content: '""',
+//     position: 'absolute',
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     backgroundImage: `url(${backgroundImage})`,
+//     backgroundSize: 'cover',
+//     backgroundPosition: 'center',
+//     opacity: 0.5,
+//   },
+// }));
+
+// const Background = () => {
+//   return <BackgroundBox />;
+// };
+
+// export default Background;
