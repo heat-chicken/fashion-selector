6fd76b849aa91e00d9890bed9911b4000db37388
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-07-19 20:44:55 -0600

    Connecting ImageFetcher with the SecretCloset
    - Created ImageFetcher component to simulate API data fetching
    - Modified SecretCloset to accept data from ImageFetcher
    - Implemented useState hook in SecretCloset to pass data/imgs down
    - Carousel only renders if an image
    - removed the hard coded data in secretCloset and in fetchImages getting them from public

--- client/src/components/ImageFetcher.jsx
+++ client/src/components/ImageFetcher.jsx
@@ -1,80 +1,47 @@
-import React, { useState, useEffect } from 'react'; 
+import React, { useEffect } from "react";
 
+import carousel1 from '../Assets/Images/secretCloset/image (1).png';
+import carousel2 from '../Assets/Images/secretCloset/Image from iOS (1).jpg';
+import carousel3 from '../Assets/Images/secretCloset/IMG_4166.jpg';
+import carousel4 from '../Assets/Images/secretCloset/IMG_20240503_114840.jpg';
+import carousel5 from '../Assets/Images/secretCloset/image (1).png';
 
+const ImageFetcher = ({ updateItems }) => {
+  useEffect(() => {
+    // Simulating an API call with setTimeout
+    setTimeout(() => {
+      const fetchedItems = [
+        {
+          photo: carousel1,
+          description: 'Photo by minhphamdesign @Unsplash',
+          artist: 'https://unsplash.com/@minhphamdesign',
+        },
+        {
+          photo: carousel2,
+          description: 'Photo by minhphamdesign @Unsplash',
+          artist: 'https://unsplash.com/@minhphamdesign',
+        },
+        {
+          photo: carousel3,
+          description: 'Photo by minhphamdesign @Unsplash',
+          artist: 'https://unsplash.com/@minhphamdesign',
+        },
+        {
+          photo: carousel4,
+          description: 'Photo by minhphamdesign @Unsplash',
+          artist: 'https://unsplash.com/@minhphamdesign',
+        },
+        {
+          photo: carousel5,
+          description: 'Photo by minhphamdesign @Unsplash',
+          artist: 'https://unsplash.com/@minhphamdesign',
+        },
+      ];
+      updateItems(fetchedItems);
+    }, 1000); // Simulate a 1-second delay
+  }, [updateItems]);
 
-// ImageFetcher fetches images from the supabase db using the users email as the key to find the image. it is wrapped in the useEffect hook to ensure that the fetch is only called once when the component mounts.
-const ImageFetcher = ({ email, updateUrls }) => {
-    const [localImageUrls, setLocalImageUrls] = useState([]); // the image urls are stored in the localImageUrls state using the setLocalImageUrls function.
-    const [imageUrls, setImageUrls] = React.useState([]); // 3) the image urls are then stored in the imageUrls state using the setImageUrls function.
+  return null; // This component doesn't render anything
+};
 
-    React.useEffect(() => {
-        const fetchImages = async () => { // 1) Image fetcher fetches images from the supabase db using the users email as the key to find the image. it is wrapped in the useEffect hook to ensure that the fetch is only called once when the component mounts.
-            try {
-                const imageUrls = [
-                    {
-                      photo: carousel1,
-                      description: 'Photo by minhphamdesign @Unsplash',
-                      artist: 'https://unsplash.com/@minhphamdesign',
-                    },
-                      {
-                      photo: carousel2,
-                      description: 'Photo by minhphamdesign @Unsplash',
-                      artist: 'https://unsplash.com/@minhphamdesign',
-                      },
-                      {
-                      photo: carousel3,
-                      description: 'Photo by minhphamdesign @Unsplash',
-                      artist: 'https://unsplash.com/@minhphamdesign',
-                      },
-                      {
-                      photo: carousel4,
-                      description: 'Photo by minhphamdesign @Unsplash',
-                      artist: 'https://unsplash.com/@minhphamdesign',
-                      },
-                      {
-                      photo: carousel5,
-                      description: 'Photo by minhphamdesign @Unsplash',
-                      artist: 'https://unsplash.com/@minhphamdesign',
-                      },
-                  
-                    // ... other imageUrls
-                  ];
-
-                  // testing to see if the imageUrls are being set to the localImageUrls state so that they can be used in the SecretCloset component.
-                  setLocalImageUrls(imageUrls);
-                  updateUrls(imageUrls);
-
-
-                // const response = await fetch(`/api/fetchImage?email=${email}`);
-
-                // if (!response.ok) {
-                //     throw new Error('Network response was not ok in ImageFetcher.jsx');
-                // }
-
-                // const data = await response.json(); // 2) response is converted to json and stored in the data variable.
-
-                // setImageUrls(data);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-        fetchImages();
-    }
-    
-    , [email, updateUrls]); // 4) the useEffect hook is dependent on the email prop, so it will be called whenever the email prop changes or when the component mounts. updateUrls is also a dependency of the useEffect hook.
-
-    return (
-        <div>
-            {imageUrls.map((imageUrl, index) => ( // 5) the image urls are mapped over and displayed in an img tag.
-                <img key={index} src={imageUrl} alt={`image-${index}`} />
-            ))}
-        </div>
-    );
-}
-
-    
-export default ImageFetcher; // 6) the ImageFetcher component is exported as the default export.
-
-// client/src/components/ImageFetcher.jsx
-    
-    
+export default ImageFetcher;
--- client/src/components/SecretCloset.jsx
+++ client/src/components/SecretCloset.jsx
@@ -4,103 +4,72 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper, Button, Typography, Grid } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import carousel1 from '../Assets/Images/secretCloset/image (1).png';
-import carousel2 from '../Assets/Images/secretCloset/Image from iOS (1).jpg';
-import carousel3 from '../Assets/Images/secretCloset/IMG_4166.jpg';
-import carousel4 from '../Assets/Images/secretCloset/IMG_20240503_114840.jpg';
-import carousel5 from '../Assets/Images/secretCloset/image (1).png';
-
-// const imageUrls = [
-//   {
-//     photo: carousel1,
-//     description: 'Photo by minhphamdesign @Unsplash',
-//     artist: 'https://unsplash.com/@minhphamdesign',
-//   },
-//     {
-//     photo: carousel2,
-//     description: 'Photo by minhphamdesign @Unsplash',
-//     artist: 'https://unsplash.com/@minhphamdesign',
-//     },
-//     {
-//     photo: carousel3,
-//     description: 'Photo by minhphamdesign @Unsplash',
-//     artist: 'https://unsplash.com/@minhphamdesign',
-//     },
-//     {
-//     photo: carousel4,
-//     description: 'Photo by minhphamdesign @Unsplash',
-//     artist: 'https://unsplash.com/@minhphamdesign',
-//     },
-//     {
-//     photo: carousel5,
-//     description: 'Photo by minhphamdesign @Unsplash',
-//     artist: 'https://unsplash.com/@minhphamdesign',
-//     },
-
-//   // ... other imageUrls
-// ];
 
 const SecretCloset = () => {
-  const [imageUrls, setImageUrls] = useState([]); // array of image URLs from ImageFetcher component, lifed from ImageFetcher component to SecretCloset component
-  const [activeIndex, setActiveIndex] = useState(0); // index of the active image in the carousel
-  console.log('imageUrls:', imageUrls);
+  const [items, setItems] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const updateImageUrls = (data) => { // function to update the imageUrls state
-    setImageUrls(data); // set the imageUrls state to the data passed in
+  const updateItems = (newItems) => {
+    setItems(newItems);
   };
-  
+
   const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : imageUrls.length - 1));
+    setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : items.length - 1));
   };
 
   const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex < imageUrls.length - 1 ? prevIndex + 1 : 0));
+    setActiveIndex((prevIndex) => (prevIndex < items.length - 1 ? prevIndex + 1 : 0));
   };
 
   return (
     <Grid container spacing={2} style={{ backgroundColor: 'transparent', margin: '15px' }}>
-      <Grid item xs={2}>
-        <SideImages imageUrls={imageUrls} start={activeIndex - 2} end={activeIndex} />
-      </Grid>
-      <Grid item xs={8}>
-        <Carousel
-          index={activeIndex}
-          onChange={(index) => setActiveIndex(index)}
-          autoPlay={false}
-          animation="slide"
-          indicators={false}
-          navButtonsAlwaysVisible
-          next={handleNext}
-          prev={handlePrev}
-          className="custom-carousel"
-        >
-          {imageUrls.map((item, index) => (
-            <Item key={index} item={item} />
-          ))}
-        </Carousel>
-        <Button onClick={handlePrev} style={{ position: 'absolute', left: '10px', top: '50%' }}>
-          <ArrowBackIosIcon />
-        </Button>
-        <Button onClick={handleNext} style={{ position: 'absolute', right: '10px', top: '50%' }}>
-          <ArrowForwardIosIcon />
-        </Button>
-      </Grid>
-      <Grid item xs={2}>
-        <SideImages imageUrls={imageUrls} start={activeIndex + 1} end={activeIndex + 3} />
-      </Grid>
+      <ImageFetcher updateItems={updateItems} />
+      {items.length > 0 && (
+        <>
+          <Grid item xs={2}>
+            <SideImages items={items} start={activeIndex - 2} end={activeIndex} />
+          </Grid>
+          <Grid item xs={8}>
+            <Carousel
+              index={activeIndex}
+              onChange={(index) => setActiveIndex(index)}
+              autoPlay={false}
+              animation="slide"
+              indicators={false}
+              navButtonsAlwaysVisible
+              next={handleNext}
+              prev={handlePrev}
+              className="custom-carousel"
+            >
+              {items.map((item, index) => (
+                <Item key={index} item={item} />
+              ))}
+            </Carousel>
+            <Button onClick={handlePrev} style={{ position: 'absolute', left: '10px', top: '50%' }}>
+              <ArrowBackIosIcon />
+            </Button>
+            <Button onClick={handleNext} style={{ position: 'absolute', right: '10px', top: '50%' }}>
+              <ArrowForwardIosIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <SideImages items={items} start={activeIndex + 1} end={activeIndex + 3} />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 };
 
-const SideImages = ({ imageUrls, start, end }) => {
-  const getWrappedIndex = (index) => (index + imageUrls.length) % imageUrls.length;
+const SideImages = ({ items, start, end }) => {
+  const getWrappedIndex = (index) => (index + items.length) % items.length;
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       {[start, start + 1, end].map((index) => (
         <img
           key={index}
-          src={imageUrls[getWrappedIndex(index)].photo}
+          src={items[getWrappedIndex(index)].photo}
           alt={`Side item ${index}`}
           style={{ width: '100%', height: 'auto', marginBottom: '10px' }}
         />
@@ -109,7 +78,7 @@ const SideImages = ({ imageUrls, start, end }) => {
   );
 };
 
-const Item = ({ item }) => { // item is an object with photo, description, and artist properties 
+const Item = ({ item }) => {
   return (
     <div className="home pages" style={{ backgroundColor: 'transparent', textAlign: 'center', padding: '1rem' }}>
       <Paper style={{ position: 'relative', margin: '10px', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '1rem' }}>
@@ -129,5 +98,3 @@ const Item = ({ item }) => { // item is an object with photo, description, and a
 };
 
 export default SecretCloset;
-
-// client/src/components/SecretCloset.jsx
