104c01f5c2e315b438700a614a417d86310258de
Micah Ziegler <micahjziegler@gmail.com>
2024-07-29 20:22:30 -0400

    playwright PR

--- tests/fashion.test.js
+++ tests/fashion.test.js
@@ -1,19 +1,21 @@
 const { test, expect } = require('@playwright/test');
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:8080/');
+});
 
-test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+test.describe('Homepage', () => {
+    test('has title', async ({ page }) => {
+        //await page.goto('http://localhost:8080/');
+
+        await expect(page).toHaveTitle("Fashion Advisor");
+    });
   
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-  });
+    test('discover your style', async ({ page }) => {
+        //await page.goto('http://localhost:8080/');
   
-  test('discover your style', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+        await page.getByRole('button', { name: 'Discover Your Style!' }).click();
   
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Discover Your Style!' }).click();
-  
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Discover Your Style' })).toBeVisible();
-  });
+        await expect(page.getByRole('heading', { name: 'Discover Your Style' })).toBeVisible();
+    });
+})
